f78797c2b152f689586557f55b5ce8575b54c349
Michael Grandorf <mgb@tdc.dk>
2019-05-27 12:37:33 +0200

    first initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lint
-Linting rules for yousee.dk
+Linting rules used for yousee.dk sites.
+
+Stylelint follows the same rules defined in [CSS & SCSS Guidelines](https://dna.yousee.dk/docs/code-guidelines/css-and-scss-guidelines.html) from YouSee DNA.
---
+++ test/eslint/test.js
@@ -0,0 +1,12 @@
+/* This file is only used for testing
+   It has style errors on purpose to test eslint works */
+
+  // no var
+  var color = '#000';
+
+  // no double quotes for strings
+  const color2 = "#000";
+
+  function MyFunction() {
+      // test
+  }
